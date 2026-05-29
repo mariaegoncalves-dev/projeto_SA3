7fa1d0fe1a6f562cf28a65540f2af13d90a0bbc6
mariaegoncalves-dev <maria.e.goncalves@edu.senai.br>
2026-05-29 13:55:49 -0300

    O começo a base do código

---
+++ sistemaEstoque.py
@@ -0,0 +1,15 @@
+produto = [
+    []
+]
+
+def adicionar_produtos():
+    idProduto= int(input("Qual o ID do produto:"))
+    nomeProduto= input("Qual o nome do produto?:")
+    quantidadeProduto= int(input("Qual a quantidadeb disponível no estoque?:"))
+    posiçãoProduto= int(input("Qual a sua posição?:"))
+adicionar_produtos()
+
+
+
+#3 [id.nome,quantidade,localização]
+#
