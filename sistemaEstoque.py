f4f5e208e94a99e77fef8de3674014d3b77af699
mariaegoncalves-dev <maria.e.goncalves@edu.senai.br>
2026-06-18 09:19:13 -0300

    Fiz as perguntas principais para achar o produto no estoque

--- sistemaEstoque.py
+++ sistemaEstoque.py
@@ -1,14 +1,22 @@
+print("----- SEJA BEM-VINDO AO SISTEMA DE ESTOQUE -----")
 produto = [
-    []
+    ["GT23","Parafuso",203,56],
+    ["BL67","Porca",421,98],
+    ["FGH83","Prego",896,76]
 ]
 
 def adicionar_produtos():
-    idProduto= int(input("Qual o ID do produto:"))
+    global produto
+    idProduto= (input("Qual o ID do produto?:"))
     nomeProduto= input("Qual o nome do produto?:")
-    quantidadeProduto= int(input("Qual a quantidadeb disponível no estoque?:"))
-    posiçãoProduto= int(input("Qual a sua posição?:"))
-adicionar_produtos()
+    quantidadeProduto= (input("Qual a quantidadeb disponível no estoque?:"))
+    posiçãoProduto= (input("Qual a sua posição?:"))
 
+    novo_produto = [idProduto, nomeProduto, quantidadeProduto,posiçãoProduto]
+
+    produto.append(novo_produto)
+    print("Produto adicionado com sucesso!")
+    adicionar_produtos()
 
 
 #3 [id.nome,quantidade,localização]
